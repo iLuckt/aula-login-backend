1a9168b760e6ddc55857e4e190648f9bca310588
iLuckt <lucaspavesi17@gmail.com>
2025-09-24 21:39:15 -0300

    .

---
+++ WebApplication1/Data/Mapeamento/AlunoMapeamento.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+public class AlunoMapeamento : IEntityTypeConfiguration<Aluno>
+{
+    public void Configure(EntityTypeBuilder<Aluno> builder)
+    {
+        builder.ToTable("Usuario");
+
+        builder.HasKey(t => t.Id);
+
+        builder.Property(t => t.Name).HasColumnType("varchar(50)");
+        builder.Property(t => t.Matricula).HasColumnType("int");
+        builder.Property(t => t.DataNascimento).HasColumnType("Date");
+        builder.Property(t => t.Cpf).HasColumnType("varchar(20)");
+        builder.Property(t => t.Cep).HasColumnType("varchar(15)");
+        builder.Property(t => t.Endereco).HasColumnType("varchar(50)");
+        builder.Property(t => t.Bairro).HasColumnType("varchar(50)");
+        builder.Property(t => t.Cidade).HasColumnType("varchar(50)");
+        builder.Property(t => t.Numero).HasColumnType("varchar(20)");
+
+    }
+}
---
+++ WebApplication1/Models/Aluno.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApplication1.Models
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Matricula { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Cpf { get; set; }
+        public string Cep { get; set; }
+        public string Endereco { get; set; }    
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Numero { get; set; }
+    }
+}
