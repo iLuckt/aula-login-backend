daf8764e52f24ffd4ce41d2d19a0cab4cce25a72
iLuckt <lucaspavesi17@gmail.com>
2025-10-24 19:34:18 -0300

    correção bug aba professores

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddDbContext<BancoContexto>(options => options.UseSqlServer(con
 
 builder.Services.AddScoped<IUsuarioRepositorio, UsuarioRepositorio>();
 builder.Services.AddScoped<IAlunoRepositorio, AlunoRepositorio>();
+builder.Services.AddScoped<IProfessorRepositorio, ProfessorRepositorio>();
 
 var app = builder.Build();
 
